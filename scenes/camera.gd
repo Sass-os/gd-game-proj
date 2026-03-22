afa80ffd46c22f88bde409124a7fe2bd3e4cd551
Sass-os <stugedam@gmail.com>
2026-03-22 18:42:17 +0200

    custom camera working

--- scenes/camera.gd
+++ scenes/camera.gd
@@ -1,1 +1,18 @@
 extends CharacterBody2D
+
+@export var limit:float
+@export var SPEED:float = 0.03
+@export var Target:CharacterBody2D
+
+
+
+func _physics_process(_delta: float) -> void:
+	var speed:float = SPEED
+	var parent_pos:Vector2 = Target.global_position
+	if global_position != parent_pos:
+		if global_position.distance_to(parent_pos) > limit:
+			global_position = lerp(global_position,parent_pos,speed*1.5)
+
+		else:
+			global_position = lerp(global_position,parent_pos,speed)
+		
